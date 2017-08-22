a660d852782d7856d5ab63ef676ad44405a6ab9e
Marco <Marco@Marco-PC>
2017-08-22 20:11:37 +0200

    exemple CamionOverviewController : affichage de la liste

--- src/devischantier/CamionOverviewController.java
+++ src/devischantier/CamionOverviewController.java
@@ -5,11 +5,15 @@
  */
 package devischantier;
 
+import db.business.FacadeDB;
+import db.dto.CamionDto;
+import db.exception.DevisChantierBusinessException;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Collection;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,6 +23,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -51,6 +56,14 @@ public class CamionOverviewController implements Initializable {
     private Button valider;
     @FXML
     private Button annuler;
+    
+    @FXML
+    private TableView idMarqueModele;
+    @FXML
+    private TableColumn idColonneMarque;    
+    @FXML
+    private TableColumn idColonneModele;
+    
 
     /**
      * Initializes the controller class.
@@ -58,6 +71,7 @@ public class CamionOverviewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        displayList();
     }
     
     @FXML
@@ -84,5 +98,16 @@ public class CamionOverviewController implements Initializable {
     private void gererSupprimer(ActionEvent event) {
     }
     
-
+    @FXML
+    private void displayList(){
+        idColonneMarque.setCellValueFactory(new PropertyValueFactory<>("marque")); //"marque" doit correspondre pile poil à l'attribut correspondant dans CamionDto
+        idColonneModele.setCellValueFactory(new PropertyValueFactory<>("modele"));
+        try {
+            Collection<CamionDto> camions = FacadeDB.getAllCamion();
+            ObservableList<CamionDto> data = FXCollections.observableArrayList(camions);
+            idMarqueModele.setItems(data);
+        } catch (DevisChantierBusinessException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
