ebdb57e84eb960053c0a8cf6e72289377175206f
Valmir Demaj <giti-adidas@hotmail.com>
2017-08-23 19:42:13 +0200

    creation des _______FormEditerController

---
+++ src/devischantier/ClientFormEditerController.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devischantier;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Vali
+ */
+public class ClientFormEditerController implements Initializable {
+
+    @FXML
+    private AnchorPane pane;
+    @FXML
+    private TextField nom;
+    @FXML
+    private TextField prenom;
+    @FXML
+    private TextField telephone;
+    @FXML
+    private TextField email;
+    @FXML
+    private DatePicker naissance;
+    @FXML
+    private Button valider;
+    @FXML
+    private Button annuler;
+    @FXML
+    private Label message;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void validation(ActionEvent event) {
+    }
+
+    @FXML
+    private void annulation(ActionEvent event) {
+    }
+    
+}
